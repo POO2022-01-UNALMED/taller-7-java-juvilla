ed0e883adc7ee3711f4cf2563b94bcb1fe8df696
juvilla <92930948+juvilla@users.noreply.github.com>
2022-05-16 17:27:43 -0500

    Add files via upload

---
+++ src/main/java/comunicacion/Alfabeto.java
@@ -0,0 +1,50 @@
+package comunicacion;
+
+public class Alfabeto extends Pictograma{
+	private String[] letras;
+	private String interpretacion;
+	
+	//constructor
+	
+	public Alfabeto(String origen,String[] letras,String interpretacion) {
+		super(origen);
+		this.letras=letras;
+		this.interpretacion=interpretacion;
+	}
+	
+	//otros metodos
+	
+	public int cantidadLetras() {
+		return letras.length;
+	}
+	
+	public String interpretacion() {
+		return interpretacion;	
+	}
+	
+	public String toString() {
+		String a="";
+		String b=", ";
+		for (int i=0;i<this.letras.length;i++) {
+			String c=this.letras[i];
+			a.concat(c);
+			a.concat(b);
+		}
+		return a;
+	}
+	
+	//metodos get y set
+	
+	public String[] getLetras(){
+		return letras;
+	}
+	public void setLetras(String[] letras) {
+		this.letras=letras;
+	}
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion=interpretacion;
+	}
+}
